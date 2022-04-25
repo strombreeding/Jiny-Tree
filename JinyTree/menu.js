643a312b56b0f8d130dc7cd4b6f1baa7c39df1dc
이진희 <104059932+strombreeding@users.noreply.github.com>
2022-04-25 15:17:58 +0900

    Add files via upload

--- JinyTree/menu.js
+++ JinyTree/menu.js
@@ -1,13 +1,24 @@
-function addice() {
-    var menu_price = document.getElementById('menu_price');
-    for (let i = 0; i <5; i++) {
-        if (menu_price.innerText == "price M=3,000원 L=4,000원") {
-            menu_price.innerText = "price M=3,500원 L=4,500원";
-          } else if (menu_price.innerText == "price M=3,500원 L=4,500원") {
-              menu_price.innerText = "price M=3,000원 L=4,000원"
-          }
-        
-        
-        
+function addice(){
+    alert('아이스 추가')
+    let iceadd = document.getElementsByClassName("menu_price");
+    for (let i = 0; i < iceadd.length; i++) {
+        iceadd[i].innerHTML = "price M=3,500원 L=4,500원";
+        }
+    let iceadd2 = document.getElementsByClassName("menu_price1");
+    for (let i = 0; i < iceadd2.length; i++) {
+        iceadd2[i].innerHTML = "price M=4,000원 L=5,000원";
+        }
+    }    
+
+function addhot(){
+
+    let hotadd = document.getElementsByClassName("menu_price");
+    for (let i = 0; i < hotadd.length; i++) {
+        hotadd[i].innerHTML = "price M=3,000원 L=4,000원";
     }
-}
+    let hotadd2 = document.getElementsByClassName("menu_price1");
+    for (let i = 0; i < hotadd2.length; i++) {
+        hotadd2[i].innerHTML = "price M=3,500원 L=4,500원";    
+        } 
+    }   
+

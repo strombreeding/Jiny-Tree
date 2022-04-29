d0877b4d78774c463fce1bb67a478a6f1a3b8c9d
이진희 <104059932+strombreeding@users.noreply.github.com>
2022-04-30 02:52:47 +0900

    Add files via upload
    *지역변수와 변수초기화를 잘 사용해야한다.
    불필요한 코드는 지움
    아이폰에서 price값이 제대로 입력이 안되는 버그발생

--- menu.js
+++ menu.js
@@ -1,4 +1,4 @@
-
+'use strict';
  //선언
 let set_arr = new Set([]);
 let total_cnt = []; //총 수량으로 ;
@@ -12,8 +12,11 @@ let for_ade_temp= 0; //order()에 사용됨
 const iceadd_name =document.getElementsByClassName("menu_name"); 
 const hot_name = document.getElementsByClassName("hidden_name");  
 //
-
-
+let obj;
+let newDiv;
+let amount;
+let default_price=3000;
+let sex;
 
 //       클릭시 메뉴 가격,이름 변경
 function addice(){
@@ -74,16 +77,16 @@ function reply_click(clicked_id){
                 }
             //장바구니 위치에 메뉴,버튼
             obj = document.getElementById("optionControl");
-            newDiv = document.createElement("div4");     
+            newDiv = document.createElement("div4"); 
+            newDiv.value = for_temp+" "+clicked_id;    
             newDiv.innerHTML = for_temp+" "+clicked_id
                         +"<form name='form'>"
                         +"<input type='button' value=' - ' onclick='del();'>"
                         +"<input type='text' class='ea_count' value='1' size='1'>"
                         +"<input type='button' value=' + ' onclick='add();'>"
-                        +"<input type='text' name='sum' style='border: none;' size='3' readonly>"
-                        +"<img src='https://github.com/strombreeding/Jiny-Tree/blob/main/img/dd.png?raw=true' size='70%;'>"
+                        +"<input type='text' name='sum' style='border: none;' size='30%' readonly>"
                         +"</form>"   
-            newDiv.setAttribute("class", "myDiv"+clicked_id);   //고유값 = innerText
+            newDiv.setAttribute("class", "myDiv"+clicked_id);   //inherent_value = innerText
             newDiv.style.backgroundColor = "rgba";      
             obj.appendChild(newDiv);
     }
@@ -93,14 +96,13 @@ function reply_click(clicked_id){
         if(for_ade_temp==1){//에이드
             order(for_temp[1]);
             let amout=0;
-            let default_price = 3000;
             let 고유값= newDiv.innerText; 
             for (let i = 0; i < menu_arr.length; i++){
-                if (i>=7&&i<=10&&고유값==menu_arr[i]){ 
-                }default_price = 3000;
+                if (고유값==menu_arr[i]){ 
+                }default_price += 0;
             }  
             for (let r = 0; r < menu_Lsize_arr.length; r++) {//L사이즈
-                if (r>=7&&r<=10&&고유값==menu_Lsize_arr[r]){  
+                if (고유값==menu_Lsize_arr[r]){  
                     default_price+=1000;
                 }  //4000
             }
@@ -109,17 +111,17 @@ function reply_click(clicked_id){
                 if(total_cnt.indexOf(고유값)==i){
                     amout=i;}
             }
-            console.log(default_price);
             ea_price[amout].value = default_price;
             if(total_cnt.includes(고유값)){ 
                 alert(고유값+"은(는) 이미 추가하셨습니다.")
                 const opControl = document.getElementById("optionControl");
-            if(scroll){opControl.scrollIntoView({behavior: "smooth", block: "center"});}
-            newDiv.remove();
+                if(scroll){opControl.scrollIntoView({behavior: "smooth", block: "center"});}
+                newDiv.remove();
             }  
             total_cnt.push(고유값); 
             set_arr.add(고유값);  
             for_ade_temp=0;
+            default_price = 3000;
 
 
 
@@ -127,10 +129,9 @@ function reply_click(clicked_id){
         }else if(add_temp==0){ //따뜻한
             order(for_temp[add_temp]);  
             let amout=0;
-            let default_price = 3000;
             let 고유값= newDiv.innerText ;
             for (let i = 0; i < menu_arr.length; i++){//따뜻한 M
-                if (i>=11&&고유값==menu_arr[i]){  
+                if (고유값==menu_arr[i]){  
                     for (let x = 0; x < premium_menu_arr.length; x++) {//비싼메뉴 선택
                         if(premium_menu_arr[x]==고유값){ //비싼메뉴가 선택됐을때
                             default_price+=500; //500을더한다.
@@ -139,7 +140,7 @@ function reply_click(clicked_id){
                 }
             }
             for (let r = 0; r < menu_Lsize_arr.length; r++) {//L사이즈
-                    if (r>=11&&고유값==menu_Lsize_arr[r]){  
+                    if (고유값==menu_Lsize_arr[r]){  
                         default_price+=1000;
                         for (let x = 0; x < premium_menu_arr.length; x++) {//비싼메뉴 선택
                             if(premium_menu_arr[x]==고유값){ //비싼메뉴가 선택됐을때
@@ -159,19 +160,18 @@ function reply_click(clicked_id){
                 const opControl = document.getElementById("optionControl");
                 if(scroll){opControl.scrollIntoView({behavior: "smooth", block: "center"});}
                 newDiv.remove();
-                default_price = 3000;
             }
             total_cnt.push(고유값); 
             set_arr.add(고유값)  
             for_ade_temp=0;
+            default_price = 3000;
 
         }else if(add_temp==1){
             order(for_temp[add_temp]);
             let amout=0;
-            let default_price = 3000;
             let 고유값= newDiv.innerText ;
             for (let i = 0; i < menu_arr.length; i++){
-                if (i<8&&고유값==menu_arr[i]){  
+                if (고유값==menu_arr[i]){  
                     default_price+=500;
                     for (let x = 0; x < premium_menu_arr.length; x++) {//비싼메뉴 선택
                         if(premium_menu_arr[x]==고유값){ //비싼메뉴가 선택됐을때
@@ -181,8 +181,8 @@ function reply_click(clicked_id){
                 } 
             }
             for (let r = 0; r < menu_Lsize_arr.length; r++) {//L사이즈
-                if (r<8&&고유값==menu_Lsize_arr[r]){  
-                    default_price+=1500;
+                if (고유값==menu_Lsize_arr[r]){  
+                    default_price+=1000;
                     for (let x = 0; x < premium_menu_arr.length; x++) {//비싼메뉴 선택
                         if(premium_menu_arr[x]==고유값){ //비싼메뉴가 선택됐을때
                             default_price+=500; //500을더한다.
@@ -191,10 +191,7 @@ function reply_click(clicked_id){
                 }
             }
             for (let i = 0; i < ea_price.length; i++) { // 클릭마다 ea_price의 length가 늘어난다.
-                amout=i
-                if(total_cnt.indexOf(고유값)==i){
-                amout=i;}
-                
+                amout=i         
             }
             ea_price[amout].value = default_price;
             if(set_arr.has(고유값)){
@@ -206,6 +203,7 @@ function reply_click(clicked_id){
             total_cnt.push(고유값); 
             set_arr.add(고유값);  
             for_ade_temp=0;
+            default_price = 3000;
   
         //
         //indexof 로 total_count배열 안에 '고유값'을 검색하여 나온 [번호]는 
@@ -213,15 +211,14 @@ function reply_click(clicked_id){
         //그 [번호]에 해당하는 곳으로 계산 값을 넣어주어야 한다. 
         }
 }
-
 /* 쓸지도 모르는 계산 로직
 function hot_price(){
     for (let i = 0; i < menu_arr.length; i++){//따뜻한 M
-        if (i>=11&&고유값==menu_arr[i]){  
+        if (i>=11&&inherent_value==menu_arr[i]){  
             default_price=3000;
             console.log(default_price);
             for (let x = 0; x < premium_menu_arr.length; x++) {//비싼메뉴 선택
-                if(premium_menu_arr[x]==고유값){ //비싼메뉴가 선택됐을때
+                if(premium_menu_arr[x]==inherent_value){ //비싼메뉴가 선택됐을때
                     default_price+=500; //500을더한다.
                     console.log(default_price);
                 }
@@ -229,11 +226,11 @@ function hot_price(){
         }
     }console.log(default_price);
     for (let r = 0; r < menu_Lsize_arr.length; r++) {//L사이즈
-            if (r>=11&&고유값==menu_Lsize_arr[r]){  
+            if (r>=11&&inherent_value==menu_Lsize_arr[r]){  
                 default_price+=1000;
                 console.log(default_price);
                 for (let x = 0; x < premium_menu_arr.length; x++) {//비싼메뉴 선택
-                    if(premium_menu_arr[x]==고유값){ //비싼메뉴가 선택됐을때
+                    if(premium_menu_arr[x]==inherent_value){ //비싼메뉴가 선택됐을때
                         default_price+=500; //500을더한다.
                         console.log(default_price);
                     }
@@ -244,11 +241,11 @@ function hot_price(){
 
 function ice_price(){
     for (let i = 0; i < menu_arr.length; i++){
-        if (i<8&&고유값==menu_arr[i]){  
+        if (i<8&&inherent_value==menu_arr[i]){  
             default_price+=500;
             console.log(default_price);
             for (let x = 0; x < premium_menu_arr.length; x++) {//비싼메뉴 선택
-                if(premium_menu_arr[x]==고유값){ //비싼메뉴가 선택됐을때
+                if(premium_menu_arr[x]==inherent_value){ //비싼메뉴가 선택됐을때
                     default_price+=500; //500을더한다.
                     console.log(default_price);
                 }
@@ -256,11 +253,11 @@ function ice_price(){
         } 
     }
     for (let r = 0; r < menu_Lsize_arr.length; r++) {//L사이즈
-        if (r<8&&고유값==menu_Lsize_arr[r]){  
+        if (r<8&&inherent_value==menu_Lsize_arr[r]){  
             default_price+=1500;
             console.log(default_price);
             for (let x = 0; x < premium_menu_arr.length; x++) {//비싼메뉴 선택
-                if(premium_menu_arr[x]==고유값){ //비싼메뉴가 선택됐을때
+                if(premium_menu_arr[x]==inherent_value){ //비싼메뉴가 선택됐을때
                     default_price+=500; //500을더한다.
                     console.log(default_price);
                 }
@@ -280,8 +277,8 @@ function ice_price(){
 // let amount;
 // init();
 //     function init () {
-//         가격 = document.form.sell_price.value; //form 안에있는 sell_price의 value 값을 sell_priceamout에 담음 ->가격
-//         amount = document.form.amount.value; //form 안에있는 amout 의 value 값을 amout 라는 이름의 amout에 담음 ->수량 조절
+//         가격 = document.form.sell_price.value; //form 안에있는 sell_price의 value 값을 sell_priceamount에 담음 ->가격
+//         amount = document.form.amount.value; //form 안에있는 amount 의 value 값을 amount 라는 이름의 amount에 담음 ->수량 조절
 //         document.form.sum.value = sell_price; // 읽기전용인 sum 의 value 값을 위에서 선언한 sell_price로 한다.
 //         change(); //change 함수 실행
 //     }
@@ -296,7 +293,7 @@ function ice_price(){
 //     function del () {
 //         hm = document.form.amount;// form 안에있는 amount 를 hm 에 담음
 //         sum = document.form.sum; // form 안에있는 sum을 sum에 담음
-//         if (hm.value > 1) { // hm의 value 가 1보다 클 경우에 -버튼을 누를경우 hm(amout)의 value를 1 줄이고
+//         if (hm.value > 1) { // hm의 value 가 1보다 클 경우에 -버튼을 누를경우 hm(amount)의 value를 1 줄이고
 //         hm.value -- ;
 //         sum.value = parseInt(hm.value) * sell_price;//sum의 value
 //         }

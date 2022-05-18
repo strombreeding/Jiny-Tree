0ad05eab5b024c2a9d4521c38f618cd939c8ef73
이진희 <104059932+strombreeding@users.noreply.github.com>
2022-05-18 12:15:48 +0900

    Add files via upload
    2022.5.18 지니나무 결제시스템  작업 종료

---
+++ JinyTree/pay_seccess.js
@@ -0,0 +1,24 @@
+"use strict";
+let clip = localStorage.getItem("영수증번호");
+let double = 0;
+let textArea;
+function run() {
+  alert("영수증 번호 : " + clip);
+}
+function copy_clip() {
+  alert(clip + " 가 클립보드에 복사되었습니다.\n결제내역조회으로 이동합니다!");
+  if (double < 1) {
+    const body = document.getElementById("board");
+    textArea = document.createElement("textarea");
+    textArea.innerHTML = clip;
+    textArea.setAttribute("id", "textArea");
+    body.appendChild(textArea);
+  }
+  let recipe = document.getElementById("textArea");
+  recipe.select();
+  document.execCommand("copy");
+  window.localStorage.removeItem("주문");
+  window.localStorage.removeItem("영수증번호");
+  location.href = "https://jineecafe.netlify.app/reward.html";
+  double += 1;
+}
